707fe2e9b835868232724d1696d28e5edc2d59f2
Yuka Ishii <ishi0026@algonquinlive.com>
2024-04-19 00:00:02 -0400

    add back to home button

--- app.vue
+++ app.vue
@@ -1,30 +1,15 @@
 <template>
-  <!-- Navigation Bar using Bootstrap -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <!-- Toggler button on the right -->
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      
-      <!-- Navbar content that collapses -->
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto"> <!-- ms-auto will keep the nav to the right -->
-          <li class="nav-item">
-            <RouterLink :to="'/'" class="nav-link">Home</RouterLink>
-          </li>
-          <li class="nav-item">
-            <RouterLink :to="'#'" class="nav-link">Review</RouterLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
   <!-- Main Content Area -->
-  <div class="row p-5 pb-4 justify-content-lg-between g-4 bg-light">
-    <h1 class="text-center col-lg-6 col-lg-text-left">Book Review Site</h1>
-    <Search class="col-lg-4" />
+  <div class="container mt-5">
+    <div class="text-center mb-4">
+      <h1>Book Review Site</h1>
+    </div>
+    <div class="d-flex justify-content-between align-items-center">
+      <!-- Search Component -->
+      <Search class="flex-grow-1 me-3" />
+      <!-- Back to Home Button -->
+      <NuxtLink to="/" class="btn btn-primary">Back to Home</NuxtLink>
+    </div>
   </div>
 
   <!-- Nuxt Page Content, where individual pages are rendered -->
@@ -35,9 +20,13 @@
 </script>
 
 <style scoped>
-.container-fluid {
+.container {
   display: flex;
-  justify-content: flex-end; /* Aligns toggler to the right */
+  flex-direction: column;
+  align-items: center;
+}
+.btn-primary:hover {
+  background-color: green; 
+  color: #ffffff; /* Optional: change text color on hover */
 }
 </style>
-
