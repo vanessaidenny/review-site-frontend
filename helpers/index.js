37b049eb3059f252d8df3a558b341f2cfbff2947
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2024-04-19 00:00:26 -0400

    Fix data fetch for review id

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://review-site-headless-cms.onrender.com/api/reviews?populate=image'
 const BASE_URL = 'https://review-site-headless-cms.onrender.com/api/reviews/'
+const POPULATE_URL = '?populate=image'
+const API_URL = BASE_URL + POPULATE_URL
 
-export { BASE_URL, API_URL }
+export { BASE_URL, POPULATE_URL, API_URL }
--- pages/reviews/[id].vue
+++ pages/reviews/[id].vue
@@ -1,14 +1,16 @@
 <template>
-    {{ data }}
+    <div class="container">
+        <h2 class="py-5">{{ data.data.attributes.title }}</h2>
+    </div>
 </template>
 
 <script setup>
-    import { BASE_URL } from '@/helpers'
+    import { BASE_URL, POPULATE_URL } from '@/helpers'
 
     const route = useRoute()
     const id = route.params.id
 
-    const { data } = await useFetch(BASE_URL + id)
+    const { data } = await useFetch(BASE_URL + id + POPULATE_URL)
 </script>
 
 <style scoped>
