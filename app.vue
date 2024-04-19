9535d0e28676c5184d8260f5776d1a664e2508d9
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2024-04-19 13:08:11 -0400

    Moved header and nav as default layout

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
-  <!-- Main Content Area -->
-  <div class="container mt-5">
-    <div class="text-center mb-4">
-      <h1>Book Review Site</h1>
-    </div>
-    <div class="d-flex justify-content-between align-items-center">
-      <!-- Search Component -->
-      <Search class="flex-grow-1 me-3" />
-      <!-- Back to Home Button -->
-      <NuxtLink to="/" class="btn btn-primary">Back to Home</NuxtLink>
-    </div>
-  </div>
-
-  <!-- Nuxt Page Content, where individual pages are rendered -->
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <script setup>
---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header class="container mt-5">
+        <div class="text-center mb-4">
+            <h1>Book Review Site</h1>
+        </div>
+        <div class="d-flex justify-content-between align-items-center">
+            <!-- Search Component -->
+            <Search class="flex-grow-1 me-3" />
+            <!-- Back to Home Button -->
+            <nav>
+                <NuxtLink to="/" class="btn btn-primary">Back to Home</NuxtLink>
+            </nav>
+        </div>
+        </header>
+  
+        <!-- Output the page content -->
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+    
+  
+<script setup>
+</script>
+
+<style scoped>
+</style>
